06ef79d9491d56aad579a078248692fdab749730
Fabio Vedovelli <vedovelli@gmail.com>
2017-11-11 09:42:46 -0200

    Highcharts adicionado e um chart pie criado em Dashboard!

--- src/features/dashboard/index.vue
+++ src/features/dashboard/index.vue
@@ -1,12 +1,85 @@
 
 <script>
+  import Highcharts from 'highcharts'
+  import { mapState, mapActions } from 'vuex'
   export default {
-    name: 'Dashboard'
+    name: 'Dashboard',
+    mounted () {
+      this.fetchCategories()
+    },
+    watch: {
+      categories () {
+        this.setupChart()
+      }
+    },
+    methods: {
+      ...mapActions('categories', {
+        fetchCategories: 'fetch'
+      }),
+      setupChart () {
+        Highcharts.chart(this.$refs.chartContainer, {
+          credits: {
+            href: 'http://www.vedovelli.com.br',
+            text: 'vedovelli.com.br'
+          },
+          chart: {
+            plotBackgroundColor: null,
+            plotBorderWidth: null,
+            plotShadow: false,
+            type: 'pie'
+          },
+          title: {
+            text: 'Quantidade de produtos por categorias'
+          },
+          tooltip: {
+            pointFormat: '{series.name}:<b>{point.y}</b>'
+          },
+          plotOptions: {
+            pie: {
+              allowPointSelect: true,
+              cursor: 'pointer',
+              dataLabels: {
+                enabled: false
+              }
+            }
+          },
+          series: [{
+            name: 'Produtos',
+            colorByPoint: true,
+            data: this.dataSource
+          }]
+        })
+      }
+    },
+    computed: {
+      ...mapState('categories', {
+        categories (state) {
+          return state.list
+        }
+      }),
+      dataSource () {
+        return this.categories.map(category => {
+          return {
+            name: category.name,
+            y: category.products.length
+          }
+        })
+      }
+    }
   }
 </script>
 
 <template>
   <div>
-    <h2>Dashboard</h2>
+    <div class="chart-container" ref="chartContainer"></div>
   </div>
 </template>
+
+<style scoped>
+  .chart-container {
+    width: 720px;
+    height: 360px;
+    margin: 0 auto;
+    background-color: #f4f4f4;
+  }
+</style>
